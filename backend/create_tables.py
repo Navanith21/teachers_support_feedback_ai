fd43940f12aceb8a037943c0b5bcc297cad139cf
Gogul <gogul2706@gmail.com>
2026-02-11 18:57:24 +0530

    Revert "Add admin authentication and user creation"
    This reverts commit b7bd8068b4b2a1cadc47f1ddf818b38dc249d47d.

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,18 +1,9 @@
 import sys
 import os
 
-from backend.db.pgdatabase import engine
-from backend.models.models import User
-# from backend.api.deps import engine
-# from backend.models.teacher_notes import Base
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-User.metadata.create_all(bind=engine)
+from backend.api.deps import engine
+from backend.models.teacher_notes import Base
 
-print("Tables created successfully")
-
-
-# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-
-
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
